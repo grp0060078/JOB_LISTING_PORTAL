4389c2eecde131ea83127e2e9d08f2ba752553cb
grp0060078 <grp006007@gmail.com>
2024-08-28 11:24:56 +0530

    commit

--- server.js
+++ server.js
@@ -29,4 +29,4 @@ mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.error('MongoDB connection error:', err));
 
-app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port https://localhost:${PORT}`));
